0fbda0c2e314356a3ab088519e889479863e0e13
Rajeev Pai <rajeevpai.code@gmail.com>
2022-01-04 07:28:14 +0530

    Fix border top for tablet and smaller

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -68,11 +68,11 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
-  border-top: 4px solid ${COLORS.gray[900]};
 
   @media ${QUERIES.tabletAndSmaller} {
     justify-content: space-between;
     align-items: center;
+    border-top: 4px solid ${COLORS.gray[900]};
   }
 
   @media ${QUERIES.phoneAndSmaller} {
